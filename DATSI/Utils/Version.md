d7571e7a1d904817cf226d5459bd1870d35836ae
Alvaro Cabo <alvarocaboac2@gmail.com>
2022-10-10 17:32:02 +0200

    Cd

--- DATSI/Utils/Version.md
+++ DATSI/Utils/Version.md
@@ -6,7 +6,20 @@ Se ha conseguido una shell que reconoce:
 - El número de mandatos introducidos
 - El número de argumentos de ellos
 - Salida de error en color rojo
+- Catch Ctrl+C y Ctrl+\
+- Exit con Ctrl+D con mensaje personalizado
 
 Además se ha implementado un promt que reconoce el path actual
 con el esquema:
 > username@PATH $
+
+#### 9/10/22
+Se ejecutan comandos externos (usr/bin) correctamente,
+obteniendo su PID del fork con su proceso padre, que les espera para morir
+
+- Se implementa exeIC(command, argc) para distinguir entre funciones internas y externas
+- Se crean distintas funciones para gestionar los distintos comandos internos:
+    - cd [YES]
+    - umask
+    - time
+    - read
--- DATSI/main.c
+++ DATSI/main.c
@@ -36,7 +36,9 @@
 #include <pwd.h>
 #include <string.h>
 #include <limits.h>
+#include <sys/wait.h>
 #include <strings.h>
+#include <stdbool.h>
 
 /******	Variables Globales	***/
 #define MAX_PATH 4096 //common linux maxsize
@@ -56,7 +58,25 @@ static char* HOME; //Path del home
 extern int obtain_order();		/* See parser.y for description */
 void PrintPromtCWD();
 void errorPrint();
-int* countArgs(char ***argvv, int argvc, char **redir, int background);
+int* countArgs(char ***argvv, int argvc);
+int cdIC(char** argv, int argc);
+int timeIC(char** argv, int argc);
+int umaskIC(char** argv, int argc);
+int readIC(char** argv, int argc);
+
+//Lista de funciones internas
+const char** ICommands ={"cd", "umask","time", "read"};
+/*Mapa de funciones internas para llamar en función
+del argumento introducido*/
+const static struct {
+  const char *name; //Command name
+  void (*func)(char **argv, int argc);
+  } function_map [] = {
+  { "cd", cdIC },
+  { "time", timeIC },
+  { "umask", umaskIC },
+  { "read", readIC },
+};
 
 /******* Main ********/
 
@@ -101,24 +121,47 @@ int main(void)
 
 		#if 1
 			/*YOU CAN COMMENT THIS SECTION BY SWITCHING THE IF*/
-			
 			char** command;
-			int* argcc = countArgs(argvv, argvc, filev, bg);
+			int* argcc = countArgs(argvv, argvc);
 			int N_commands=0; 
-
+	
 			while((command=argvv[N_commands])){
 				int n_args=argcc[N_commands] - 1; //Le quitamos el nombre del comando
-            	fprintf(stderr, "Procesando mandato numero %d, \"%s\" con %d argumento/s\n", argvc, command[0], n_args);
+            	fprintf(stderr, "#%d: \"%s\" using %d argument/s\n", N_commands, command[0], n_args);
 				N_commands++;
-			}
 
-		#endif
+			/*Now we gotta discriminate wether the command is:
+				internal: */
+			if (exeIC(command, n_args) == -1){
+                pid_t pid;
+                switch (pid = fork ()) {
+                    case - 1 : printError("fork de mandato"); exit(1);
+                    case   0 : 
+						/* proceso hijo */
+                        execvp(command [0], command);
+                        //Error in exec
+                        printError("exec error"); exit(1);
+		            default: /* padre */
+                        /* establece el manejador */
+                        waitpid(pid, NULL, 0); //espera por su hijo
+                	}
+            	}
+			
+
+			}
+	#endif
 	}
 	exit(0);
 	return 0;
 }
 
 /*FUNCIONES AUXILIARES*/
+
+void errorPrint(char *err) {
+    fprintf(stderr, RED);
+    perror(err);
+    fprintf(stderr, RESET); 
+}
 
 void PrintPromtCWD(char* CWD){
 	//Prompt: user@PATH$
@@ -157,7 +200,7 @@ void printError(char *error) {
     fprintf(stderr, RESET);
 }
 
-int* countArgs(char ***argvv, int argvc, char **redir, int background){
+int* countArgs(char ***argvv, int argvc){
 	
 	int* res = malloc(sizeof(int)*argvc); 
 	int i=0;
@@ -174,4 +217,63 @@ int* countArgs(char ***argvv, int argvc, char **redir, int background){
 	}
 	return res;
 	
+}
+
+/*INTERNAL COMMANDS SELECT AND IMPLEMENTATION*/
+int exeIC(char ** command, int argc)
+{
+	/*Given a command name returns its function*/
+
+  for (int i = 0; i < (sizeof(function_map) / sizeof(function_map[0])); i++) {
+    if (!strcmp(function_map[i].name, command[0]) && function_map[i].func) {
+		//Ejecuta la función
+      function_map[i].func(command, argc);
+      return 0;
+    }
+  }
+  return -1;
+}
+
+/* Used the --help section of these command to complete the 
+	closest implementation for this minishell */
+
+int cdIC(char ** command, int argc){
+	/*
+	@param: [dir] (argc <=2)
+		* if length(command) == 1 -> dir = $HOME
+	@return 0 if $PWD changed, -1 otherwise
+	*/
+
+	char* dir;
+
+	switch (argc)
+	{
+	case 0:
+		/* dir = $HOME */
+		dir=getenv("HOME");
+		break;
+	case 1:
+		dir=command[1];
+		break;
+	default:
+		errorPrint("Cant have more than 1 argument for 'CD'");
+		return -1;
+	}
+
+	//Check for permisions and other errors when we changhe $PWD
+	if(chdir(dir)==0){
+		fprintf(stdout, "%s\n", CWD);
+	}else{
+		errorPrint("Can't access directory");
+		return -2;
+	}
+}
+int timeIC(char** argv, int argc){
+	return -1;
+}
+int umaskIC(char** argv, int argc){
+	return -1;
+}
+int readIC(char** argv, int argc){
+	return -1;
 }
